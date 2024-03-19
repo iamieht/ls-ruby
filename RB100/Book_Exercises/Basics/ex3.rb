f6d378192afe5734e14278d285f43614c7dde875
Iván E. Hernández T <iamieht@gmail.com>
2024-03-19 11:01:19 +0100

    Create ex3.rb
    Ex3

---
+++ RB100/Book_Exercises/Basics/ex3.rb
@@ -0,0 +1,23 @@
+movies = {
+  :platoon => 1990,
+  :toy_story => 1995,
+  :pulp_fiction => 1994,
+  :toy_story_2 => 1998
+}
+
+puts movies[:platoon]
+puts movies[:toy_story]
+puts movies[:pulp_fiction]
+puts movies[:toy_story_2]
+
+movies = { jaws: 1975,
+           anchorman: 2004,
+           man_of_steel: 2013,
+           a_beautiful_mind: 2001,
+           the_evil_dead: 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
