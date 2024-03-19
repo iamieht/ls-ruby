f5a2ece69bba5dcbfa409b33d653f82a8e925034
Iván E. Hernández T <iamieht@gmail.com>
2024-03-19 11:09:45 +0100

    Create ex5.rb
    Ex5

---
+++ RB100/Book_Exercises/Basics/ex5.rb
@@ -0,0 +1,5 @@
+# Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
+puts "Factorial of 5: #{5 * 4 * 3 * 2 * 1}"
+puts "Factorial of 6: #{6 * 5 * 4 * 3 * 2 * 1}"
+puts "Factorial of 7: #{7 * 6 * 5 * 4 * 3 * 2 * 1}"
+puts "Factorial of 8: #{8 * 7 * 6 * 5 * 4 * 3 * 2 * 1}"
