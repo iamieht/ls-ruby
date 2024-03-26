10448410a026ffff6507a57fe31ee479c618c980
Iván E. Hernández T <iamieht@gmail.com>
2024-03-26 07:41:37 +0100

    Create ex2.rb
    Ex2 - While loops

---
+++ RB100/Ruby_Basics/1.Reading_Documentation/ex2.rb
@@ -0,0 +1,3 @@
+# Locate the description of the while loop in the ruby documentation.
+# https://ruby-doc.org/docs/ruby-doc-bundle/Tutorial/part_02/while.html
+# https://docs.ruby-lang.org/en/3.3/syntax/control_expressions_rdoc.html#label-while+Loop
