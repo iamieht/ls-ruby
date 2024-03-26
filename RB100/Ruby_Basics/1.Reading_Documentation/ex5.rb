8623fdafd023100b0c2bfdaaa8de71f91a6c6d1d
Iván E. Hernández T <iamieht@gmail.com>
2024-03-26 07:57:25 +0100

    Create ex5.rb
    Ex5 - Large Numbers

---
+++ RB100/Ruby_Basics/1.Reading_Documentation/ex5.rb
@@ -0,0 +1,11 @@
+# Using the ruby documentation, determine how you can write large numbers in a way that makes them easier to read.
+
+# https://docs.ruby-lang.org/en/3.3/syntax/literals_rdoc.html
+# The underscore may be used to enhance readability for humans. You may place an underscore anywhere in the number.
+
+1234
+1_234
+
+# Further Exploration
+# Is it okay to write the above number as 19_87_65_4321?
+# Yes, it will represent the number 1987654321
