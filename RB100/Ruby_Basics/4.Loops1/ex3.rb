c7bda61352107731bbf3cfb7eb795932dda568d7
Iván E. Hernández T <iamieht@gmail.com>
2024-03-26 13:24:23 +0100

    Create ex3.rb
    Control the Loop

---
+++ RB100/Ruby_Basics/4.Loops1/ex3.rb
@@ -0,0 +1,16 @@
+# Modify the following loop so it iterates 5 times instead of just once.
+iterations = 1
+
+loop do
+  puts "Number of iterations = #{iterations}"
+  iterations += 1
+  break if iterations > 5 
+end
+
+# Further Exploration
+iterations = 1
+loop do
+  puts "Number of iterations = #{iterations}"
+  break if iterations > 4
+  iterations += 1   
+end
