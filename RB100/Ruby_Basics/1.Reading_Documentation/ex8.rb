4b1f990528b591af61575e9d850e073f2519493b
Iván E. Hernández T <iamieht@gmail.com>
2024-03-26 08:10:32 +0100

    Create ex8.rb
    Ex8 - Right Justifying Strings

---
+++ RB100/Ruby_Basics/1.Reading_Documentation/ex8.rb
@@ -0,0 +1,5 @@
+# Use the ruby documentation for the String class to determine which method can be used to right justify a String object.
+
+# https://docs.ruby-lang.org/en/master/String.html#method-i-rjust
+
+'hello'.rjust(10)       # => "     hello"
