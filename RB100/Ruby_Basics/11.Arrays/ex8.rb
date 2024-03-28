186eaa24b23c0733205732979bdef04d15fc6c88
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-03-28 19:16:32 +0100

    Arrays - Ex8

---
+++ RB100/Ruby_Basics/11.Arrays/ex8.rb
@@ -0,0 +1,3 @@
+# Group each name with the number following it by placing the pair in their own array. Then create a nested array containing all three groups. 
+
+[['Dave', 7], ['Miranda', 3], ['Jason', 11]]
