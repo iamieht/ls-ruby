1f5eb43f866b1611da3374e8a6db3616c4068a51
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-03-28 13:47:30 +0100

    Methods - Ex5

---
+++ RB100/Ruby_Basics/7.Methods/ex5.rb
@@ -0,0 +1,6 @@
+# write a method called car that takes two arguments and prints a string containing the values of both arguments.
+def car(str1, str2)
+  puts "#{str1} #{str2}"
+end
+
+car('Toyota', 'Corolla')
