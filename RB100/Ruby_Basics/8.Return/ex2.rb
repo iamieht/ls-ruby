e405d2608acfaffc885b4e9e1c73bef5192a3f18
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-03-28 14:05:01 +0100

    Return - Ex2

---
+++ RB100/Ruby_Basics/8.Return/ex2.rb
@@ -0,0 +1,9 @@
+# What will the following code print? Why? 
+
+def meal
+  'Evening'
+end
+
+puts meal # => Evening
+
+# the return value of meal will be "Evening" because it's the only line in the method.
