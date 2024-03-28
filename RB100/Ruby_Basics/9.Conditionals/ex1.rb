4f7f06a6513e8077fe569430d21c19af27007fa4
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-03-28 14:33:25 +0100

    Conditionals - Ex1

---
+++ RB100/Ruby_Basics/9.Conditionals/ex1.rb
@@ -0,0 +1,7 @@
+# Write an if statement that prints "The sun is so bright!" if sun equals 'visible'.
+
+sun = ['visible', 'hidden'].sample
+
+if sun == 'visible'
+  puts "The sun is so bright!"
+end
