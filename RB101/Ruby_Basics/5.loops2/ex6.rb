5ae6c255f689eeb1630d4f439958e21d5d19a690
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-24 08:28:15 +0200

    Create ex6.rb
    Empty the Array

---
+++ RB101/Ruby_Basics/5.loops2/ex6.rb
@@ -0,0 +1,16 @@
+# Empty the Array
+# Given the array below, use loop to remove and print each name from first to last. Stop the loop once names doesn't contain any more elements.
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+loop do
+  puts names.shift
+  break if names.empty?
+end
+
+puts "*" * 20
+
+# "Further Exploration"
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+loop do
+  puts names.pop
+  break if names.empty?
+end
