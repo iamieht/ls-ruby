3ed73497f2e9ed7427537ecb4c9734997925a9bb
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-04-25 19:41:03 +0200

    Print Me (Part 2)

---
+++ RB101/Ruby_Basics/7.methods/ex2.rb
@@ -0,0 +1,8 @@
+# Print Me (Part 2)
+# Write a method named print_me so that I'm printing the return value! is printed
+
+def print_me
+  return "I'm printing the return value!"
+end
+
+puts print_me
