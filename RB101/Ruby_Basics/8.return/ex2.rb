76dfb100d0a97dcfec634f78eb03f6602f198c02
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-26 08:42:16 +0200

    Create ex2.rb
    Breakfast, Lunch, or Dinner? (Part 2)

---
+++ RB101/Ruby_Basics/8.return/ex2.rb
@@ -0,0 +1,12 @@
+# Breakfast, Lunch, or Dinner? (Part 2)
+# What will the following code print? Why?
+
+def meal
+  'Evening'
+end
+
+puts meal
+
+# The output of the meal invocation is the string value 'Evening' as it is the last executed expression in the method.
+
+# The last exercise explained how a method's return value is determined. Therefore, we can tell that the return value of meal will be "Evening" because it's the only line in the method.
