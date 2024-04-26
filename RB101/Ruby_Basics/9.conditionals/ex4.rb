625ab2e06947c7f3d7de08fbcba0afee0009be90
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-26 09:18:31 +0200

    Create ex4.rb
    True or False

---
+++ RB101/Ruby_Basics/9.conditionals/ex4.rb
@@ -0,0 +1,6 @@
+# True or False
+# Write a ternary operator that prints "I'm true!" if boolean equals true and prints "I'm false!" if boolean equals false.
+
+boolean = [true, false].sample
+
+puts boolean ? "I'm true" : "I'm false"
