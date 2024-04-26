241a0d0c86f96389604a338923d0d65a8db7b27f
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-26 09:24:47 +0200

    Create ex6.rb
    Stoplight (Part 1)

---
+++ RB101/Ruby_Basics/9.conditionals/ex6.rb
@@ -0,0 +1,10 @@
+# Stoplight (Part 1)
+# Write a case statement that prints "Go!" if stoplight equals 'green', "Slow down!" if stoplight equals 'yellow', and "Stop!" if stoplight equals 'red'.
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green' then puts "Go!"
+when 'yellow' then puts "Slow down!"
+else puts "Stop!"
+end
