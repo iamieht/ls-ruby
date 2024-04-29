2fa4eac7912924aed115a18684ad4149f318da18
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-29 08:44:31 +0200

    Create ex9.rb
    Favorite Number (Part 2)

---
+++ RB101/Ruby_Basics/11.arrays/ex9.rb
@@ -0,0 +1,6 @@
+# Favorite Number (Part 2)
+# Flatten and print this array. That is, the printed result should not have any subarrays, but should have all of the original strings and numbers from the original array
+
+favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+favorites.flatten!
+p favorites
