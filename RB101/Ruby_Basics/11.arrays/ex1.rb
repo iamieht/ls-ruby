676ab5f596335090ef6876053ba3c36de6180cdd
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-29 08:12:50 +0200

    Create ex1.rb
    New Pet

---
+++ RB101/Ruby_Basics/11.arrays/ex1.rb
@@ -0,0 +1,8 @@
+# New Pet
+# Select 'fish' from pets, assign the return value to a variable named my_pet, then print the value of my_pet.
+
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+my_pet = pets[2]
+puts "I have a #{my_pet}"
+
