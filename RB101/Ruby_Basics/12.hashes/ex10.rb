e679d26b6f42ce463a1b7c97dbed073b7caf4dca
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-29 09:30:13 +0200

    Create ex10.rb
    Which Collection?

---
+++ RB101/Ruby_Basics/12.hashes/ex10.rb
@@ -0,0 +1,10 @@
+# Which Collection?
+# Rewrite car as a nested array containing the same key-value pairs.
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+
+car_array = [[:type, 'sedan'], [:color, 'blue'], [:year, 2003]]
+p car_array
