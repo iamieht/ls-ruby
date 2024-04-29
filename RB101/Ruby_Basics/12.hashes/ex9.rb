00760f08fd4a79f879f75d046110acf2aaf1aca4
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-29 09:27:40 +0200

    Create ex9.rb
    Multiple Cars

---
+++ RB101/Ruby_Basics/12.hashes/ex9.rb
@@ -0,0 +1,17 @@
+# Multiple Cars
+# Create a nested hash
+
+multiple_cars = {
+  car: {
+    type:   'sedan',
+    color:  'blue',
+    year:   2003
+  },
+  truck: {
+    type:   'pickup',
+    color:  'red',
+    year:   1998
+  }
+}
+
+p multiple_cars
