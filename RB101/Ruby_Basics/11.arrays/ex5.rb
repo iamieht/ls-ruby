6051c2e2819f2a55acb7f5a4834e3797887accbd
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-29 08:34:04 +0200

    Create ex5.rb
    What Color Are You?

---
+++ RB101/Ruby_Basics/11.arrays/ex5.rb
@@ -0,0 +1,5 @@
+# What Color Are You?
+# Use Array#each to iterate over colors and print each element.
+colors = ['red', 'yellow', 'purple', 'green']
+
+colors.each { |color| puts "I'm the color #{color}!" }
