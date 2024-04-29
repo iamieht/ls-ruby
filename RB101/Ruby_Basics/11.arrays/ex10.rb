e12b496d2c419eed76d737dcb62fef6c4d927d48
Iván E. Hernández T. <iamieht@gmail.com>
2024-04-29 08:48:29 +0200

    Create ex10.rb
    Are We The Same?

---
+++ RB101/Ruby_Basics/11.arrays/ex10.rb
@@ -0,0 +1,8 @@
+# Are We The Same?
+# Compare array1 and array2 and print true or false based on whether they match.
+
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
+puts array1 == array2
+puts array1.eql?(array2)
