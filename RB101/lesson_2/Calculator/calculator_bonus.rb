53ef67ab42d570a25545387d8a73feaab3b15043
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-05-15 21:07:29 +0200

    Calculator Bonus - helper functions

--- RB101/lesson_2/Calculator/calculator_bonus.rb
+++ RB101/lesson_2/Calculator/calculator_bonus.rb
@@ -1,4 +1,6 @@
 # Assignment: Calculator Bonus Features
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
 def prompt(message)
   puts "=> #{message}"
@@ -17,13 +19,25 @@ def operation_to_message(operator)
   end
 end
 
+prompt(MESSAGES['welcome'])
+name = nil
+loop do
+  name = gets.chomp
+
+  if name.empty?
+    prompt(MESSAGES['valid_name'])
+  else
+    break
+  end
+end
+
 ## Examples/Test Cases
 # prompt("Welcome to Calculator with Bonus Features!!!")
 # puts number?("a")
 # puts number?(0)
 # puts number?(1)
 # puts number?(1.5)
-puts operation_to_message("1")
-puts operation_to_message("2")
-puts operation_to_message("3")
-puts operation_to_message("4")
+# puts operation_to_message("1")
+# puts operation_to_message("2")
+# puts operation_to_message("3")
+# puts operation_to_message("4")
