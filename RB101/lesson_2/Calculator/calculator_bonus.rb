d5d7d8624c4d79231c373442b4e90eb987009ce7
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-05-15 21:32:58 +0200

    Calculator Bonus - final

--- RB101/lesson_2/Calculator/calculator_bonus.rb
+++ RB101/lesson_2/Calculator/calculator_bonus.rb
@@ -1,13 +1,26 @@
 # Assignment: Calculator Bonus Features
 require 'yaml'
 MESSAGES = YAML.load_file('calculator_messages.yml')
+LANGUAGE = 'es'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
 
 def prompt(message)
   puts "=> #{message}"
 end
 
+def integer?(number)
+  number.to_i.to_s == number
+end
+
+def float?(number)
+  number.to_f.to_s == input
+end
+
 def number?(number)
-  number.instance_of?(Float) || number.instance_of?(Integer)
+  integer?(number) || float?(number)
 end
 
 def operation_to_message(operator)
@@ -19,7 +32,7 @@ def operation_to_message(operator)
   end
 end
 
-prompt(MESSAGES['welcome'])
+prompt(messages('welcome', LANGUAGE))
 name = nil
 loop do
   name = gets.chomp
@@ -30,6 +43,72 @@ loop do
     break
   end
 end
+
+prompt("Hi #{name}!")
+
+loop do # main loop
+  number1 = nil
+  loop do
+    prompt(messages('first_number', LANGUAGE))
+    number1 = gets.chomp
+
+    if number?(number1)
+      break
+    else
+      prompt(messages('valid_number', LANGUAGE))
+    end
+  end
+
+  number2 = nil
+  loop do
+    prompt(messages('second_number', LANGUAGE))
+    number2 = gets.chomp
+
+    if number?(number2)
+      break
+    else
+      prompt(messages('valid_number', LANGUAGE))
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = nil
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt(messages('operator', LANGUAGE))
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when '1' then number1.to_i() + number2.to_i()
+           when '2' then number1.to_i() - number2.to_i()
+           when '3' then number1.to_i() * number2.to_i()
+           when '4' then number1.to_f() / number2.to_f()
+           end
+
+  prompt("The result is #{result}")
+
+  prompt(messages('calculate_again', LANGUAGE))
+  answer = gets.chomp
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt(messages('goodbye', LANGUAGE))
 
 ## Examples/Test Cases
 # prompt("Welcome to Calculator with Bonus Features!!!")
