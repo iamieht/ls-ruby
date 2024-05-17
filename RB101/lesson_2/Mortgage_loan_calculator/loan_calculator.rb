c49c6127d54778613e2f70becb5e6277b0e70fe6
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-05-17 20:30:26 +0200

    Assignment: Loan Calculator init

---
+++ RB101/lesson_2/Mortgage_loan_calculator/loan_calculator.rb
@@ -0,0 +1,43 @@
+# Assignment: Mortgage / Car Loan Calculator
+# Author: Ivan E. Hernandez T.
+# Launch School: RB101
+
+### Helper Functions ####
+def messages(message, lang='en')
+end
+
+def prompt(key)
+end
+
+def user_input(ask)
+end
+
+def ask_loan_amount()
+end
+
+def ask_loan_duration()
+end
+
+def ask_interest_rate()
+end
+
+def calc_monthly_interest_rate(apr)
+end
+
+def calc_loan(loan_amount, monthly_int_rate, loan_duration)
+end
+
+def amount?(amount)
+end
+
+def month?(month)
+end
+
+def interest_rate?(interest_rate)
+end
+
+### Main Program ##########
+def main()
+end
+
+main
