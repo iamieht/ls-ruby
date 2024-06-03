613023dfea458a09d13f6474272c96f6d289360e
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-06-03 22:00:19 +0200

    Easy_3 - Ex4 multiply

---
+++ RB101/lesson_1/Small_Problems/Easy_3/ex4.rb
@@ -0,0 +1,29 @@
+## Problem (P)
+# Multiplying Two Numbers
+# Create a method that takes two arguments, multiplies them together, and returns the result.
+
+# - Inputs:
+## - 2 numbers
+# - Outputs:
+## - a number
+# - Explicit Rules:
+## - number must be multiplied
+# - Implicit Rules:
+
+
+## Examples/Test Cases (E)
+# multiply(5, 3) == 15
+
+## Data Structures
+# - Integers
+
+## Algorithm
+## - Define a method multiply that takes two arguments (num1, num2) of Numeric type
+##  - num1 * num2
+
+## Code
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(5, 3) == 15
