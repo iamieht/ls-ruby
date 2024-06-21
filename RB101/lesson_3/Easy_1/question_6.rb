c5c19208fddefcda1080c4a916851fe812d93219
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-06-21 20:44:13 +0200

    Easy 1 Problems -  question 6

---
+++ RB101/lesson_3/Easy_1/question_6.rb
@@ -0,0 +1,8 @@
+# Starting with the string:
+famous_words = "seven years ago..."
+
+# show two different ways to put the expected "Four score and " in front of it.
+
+puts "Four score and " << famous_words
+puts "Four score and " + "#{famous_words}"
+puts famous_words.prepend("Four score and ")
