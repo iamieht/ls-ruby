4b668276859e6f4e13d484e383087e3f6469c6c2
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-06-22 14:55:45 +0200

    Easy 2 - question 5

---
+++ RB101/lesson_3/Easy_2/question_5.rb
@@ -0,0 +1,6 @@
+# Show an easier way to write this array:
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+flintstones_easy = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones
+p flintstones_easy
