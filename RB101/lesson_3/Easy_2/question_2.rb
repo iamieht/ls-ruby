6f0ae456f752ba83f5d2dd7788336fec0b7f475c
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-06-22 09:40:01 +0200

    Easy 2 - question 2

---
+++ RB101/lesson_3/Easy_2/question_2.rb
@@ -0,0 +1,8 @@
+munsters_description = "The Munsters are creepy in a good way."
+
+# Convert the string in the following ways (code will be executed on original munsters_description above):
+
+puts munsters_description.swapcase    # "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+puts munsters_description.downcase    # "The munsters are creepy in a good way."
+puts munsters_description.capitalize  # "The munsters are creepy in a good way."
+puts munsters_description.upcase      # "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
