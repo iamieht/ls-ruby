566a6b2be64b1d93c56ca34dd8fb26c88c9d179c
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-07-10 19:02:05 +0200

    Ex6

---
+++ RB109/practice/1.the_basics/ex6.rb
@@ -0,0 +1,11 @@
+=begin
+Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+=end
+
+def square(num)
+  num * num
+end
+
+puts square(3.14)   #=> 9.8596
+puts square(14.11)  #=> 199.0921
+puts square(0.25)   #=> 0.0625
