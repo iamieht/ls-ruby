591572cb8b9c7b1708a7ec1b035a40c0bd128845
Iván E. Hernández T. <iamieht@gmail.com>
2024-07-10 10:45:35 +0200

    Create 1.ex1.rb
    The Basics ex1

---
+++ RB109/practice/1.the_basics/1.ex1.rb
@@ -0,0 +1,14 @@
+=begin
+Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+=end
+
+puts "Ivan" + " Hernandez"
+
+=begin
+Code Explanation
+
+On line 5, the `#+` method is called on the string object "Ivan", passing in the String object " Hernandez" as an argument. The method invocation returns a new string object "Ivan Hernandez" that is passed as an argument to the puts method invocation.
+
+The output is "Ivan Hernandez"
+The return value is `nil`
+=end
