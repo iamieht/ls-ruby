1106442cc5b61c10c30cb8cc001a0b0c7a179125
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-07-10 18:52:09 +0200

    Ex4

---
+++ RB109/practice/1.the_basics/ex4.rb
@@ -0,0 +1,7 @@
+=begin
+Use the dates from the previous example and store them in an array. Then make your program output the same thing as exercise 3.
+=end
+
+dates = [1994, 1972, 2008, 1974, 1957, 1993]
+
+dates.each { |date| puts date }
