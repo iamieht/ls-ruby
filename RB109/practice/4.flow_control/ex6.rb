292551fe9b79c964e305d6630b26900e48d659db
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-07-13 22:28:31 +0200

    flow control ex6

---
+++ RB109/practice/4.flow_control/ex6.rb
@@ -0,0 +1,10 @@
+=begin
+Write down whether the following expressions return true or false or raise an error.
+=end
+
+(32 * 4) >= "129"     # Argument Error
+847 == '847'          # false
+'847' < '846'         # false
+'847' > '846'         # true
+'847' > '8478'        # false
+'847' < '8478'        # true
