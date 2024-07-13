efd4e05dbb6744fa5df0075396a629578552687b
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-07-13 22:25:57 +0200

    flow control ex5

---
+++ RB109/practice/4.flow_control/ex5.rb
@@ -0,0 +1,21 @@
+=begin
+When you run the following code... You get an error message, why and how to fix it?
+=end
+
+# def equal_to_four(x)
+#   if x == 4
+#     puts "yup"
+#   else
+#     puts "nope"
+# end
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(5)
+
