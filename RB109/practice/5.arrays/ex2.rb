c409752e4d251e2c63079f5e7cd08b38c6d64d40
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-07-15 19:28:14 +0200

    arrays - ex2

---
+++ RB109/practice/5.arrays/ex2.rb
@@ -0,0 +1,11 @@
+=begin
+What will the following programs return? What is the value of arr after each?
+=end
+
+arr = ["b", "a"]
+arr = arr.product(Array(1..3))
+arr.first.delete(arr.first.last)
+
+arr = ["b", "a"]
+arr = arr.product([Array(1..3)])
+arr.first.delete(arr.first.last)
