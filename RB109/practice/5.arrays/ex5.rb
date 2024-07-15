caba96fbdda20b542dd3e8b2256cdd9b0590ab6f
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-07-15 19:33:47 +0200

    arrays - ex5

---
+++ RB109/practice/5.arrays/ex5.rb
@@ -0,0 +1,12 @@
+=begin
+What is the value of a, b, and c in the following program?
+=end
+
+string = "Welcome to America!"
+a = string[6]   #=> e
+b = string[11]  #=> A
+c = string[19]  #=> `nil`
+
+p a
+p b
+p c
