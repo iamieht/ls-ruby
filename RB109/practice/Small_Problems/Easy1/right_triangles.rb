700df1c974c66f55b9936cd69482b2ea7499d943
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-09-04 09:17:24 +0200

    Right Triangles - Further Exploration - Better Solution

--- RB109/practice/Small_Problems/Easy1/right_triangles.rb
+++ RB109/practice/Small_Problems/Easy1/right_triangles.rb
@@ -63,6 +63,20 @@ def triangle_v2(num, angle, orientation="up")
   end
 end
 
+## Further Exploration - Bob Rodes solution
+
+def triangle(size, right_angle)
+  case right_angle
+  when 'top_left'
+    size.downto(1) { |n| puts ('*' * n) }
+  when 'top_right'
+    size.downto(1) { |n| puts ('*' * n).rjust(size, ' ') }
+  when 'bottom_left'
+    1.upto(size) { |n| puts ('*' * n) }
+  when 'bottom_right'
+    1.upto(size) { |n| puts ('*' * n).rjust(size, ' ') }
+  end
+end
 
 
 # Examples
