8a8a8ac83e4995548bd34454d36e5782019f7c07
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-09-08 08:01:24 +0200

    Easy2 - Greeting a user

---
+++ RB109/practice/Small_Problems/Easy2/greeting_a_user.rb
@@ -0,0 +1,20 @@
+=begin
+Write a program that will ask for user's name. The program will then greet the user. If the user writes "name!" then the computer yells back to the user.
+=end
+print "What is your name? "
+name = gets.chomp!
+
+if name.end_with?('!')
+  puts "HELLO #{name.upcase.chop!}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
+
+
+#Examples
+
+# What is your name? Bob
+# Hello Bob.
+
+# What is your name? Bob!
+# HELLO BOB. WHY ARE WE SCREAMING?
