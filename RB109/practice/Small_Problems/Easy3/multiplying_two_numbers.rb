a7800d6ebfacc0cc102a45c8fdd2f1c63248440a
Iván E. Hernández T. <iamieht@gmail.com>
2024-09-17 15:58:54 +0200

    Create multiplying_two_numbers.rb
    Multiplying Two Numbers

---
+++ RB109/practice/Small_Problems/Easy3/multiplying_two_numbers.rb
@@ -0,0 +1,11 @@
+=begin
+Multiplying Two Numbers
+
+Create a method that takes two arguments, multiplies them together, and returns the result.
+=end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts multiply(5, 3) == 15
