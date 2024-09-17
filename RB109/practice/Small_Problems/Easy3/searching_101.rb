edd771e532e01d87603a89e675926f0fdddb2eb2
Iván E. Hernández T. <iamieht@gmail.com>
2024-09-17 15:33:18 +0200

    Update searching_101.rb
    Small problems - Easy3 - Searching 101

--- RB109/practice/Small_Problems/Easy3/searching_101.rb
+++ RB109/practice/Small_Problems/Easy3/searching_101.rb
@@ -4,28 +4,32 @@ Write a program that solicits 6 numbers from the user, then prints a message tha
 =end
 numbers = []
 
-while numbers.size < 6
-  if numbers.size == 5
-    puts "Enter the last number:"
-    number = gets.chomp.to_i
-    numbers << number
-    puts(numbers.include?(number) ? "The number #{number} appears in #{numbers}" :
-                                    "The number #{number} does not appear in #{numbers}")
-  else
-    puts "Enter the #{numbers.size + 1} number:"
-    number = gets.chomp.to_i
-    numbers << number
-  end
+puts "Enter the 1st number: "
+number = gets.chomp.to_i
+numbers << number
+
+puts "Enter the 2nd number: "
+number = gets.chomp.to_i
+numbers << number
+
+puts "Enter the 3rd number: "
+number = gets.chomp.to_i
+numbers << number
+
+puts "Enter the 4th number: "
+number = gets.chomp.to_i
+numbers << number
+
+puts "Enter the 5th number: "
+number = gets.chomp.to_i
+numbers << number
+
+puts "Enter the last number: "
+number = gets.chomp.to_i
+
+if numbers.include?(number)
+  puts "The number #{number} appears in #{numbers}"
+else
+  puts "The number #{number} does not appear in #{numbers}"
 end
 
-# 6.times do |num|
-#   if num == numbers.size
-#     puts "Enter the last number:"
-#   else
-#     puts "Enter the #{num + 1} number:"
-#     number = gets.chomp.to_i
-#     numbers << number
-#   end
-# end
-
-p numbers
