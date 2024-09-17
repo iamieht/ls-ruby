0eed05bd42357c59dfa8402672e6dc6655c25308
Iván E. Hernández T. <iamieht@gmail.com>
2024-09-17 15:38:34 +0200

    Create artihmetic_integer.rb
    Arithmetic Integer

---
+++ RB109/practice/Small_Problems/Easy3/artihmetic_integer.rb
@@ -0,0 +1,16 @@
+=begin
+Write a program that prompts the user for two positive integers, and then prints the results of the following operations on those two numbers: addition, subtraction, product, quotient, remainder, and power. Do not worry about validating the input.
+=end
+
+puts "Enter the first number:"
+first_number = gets.chomp.to_i
+
+puts "Enter the second number:"
+second_number = gets.chomp.to_i
+
+puts "#{first_number} + #{second_number} = #{first_number + second_number}"
+puts "#{first_number} - #{second_number} = #{first_number - second_number}"
+puts "#{first_number} * #{second_number} = #{first_number * second_number}"
+puts "#{first_number} / #{second_number} = #{first_number / second_number}"
+puts "#{first_number} % #{second_number} = #{first_number % second_number}"
+puts "#{first_number} ** #{second_number} = #{first_number ** second_number}"
