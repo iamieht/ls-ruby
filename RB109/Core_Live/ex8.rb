d3386dfdd5072cd1652b065b919b59677b6f3544
Iván E. Hernández T. <iamieht@gmail.com>
2024-09-19 18:01:29 +0200

    Create ex8.rb

---
+++ RB109/Core_Live/ex8.rb
@@ -0,0 +1,44 @@
+=begin
+Session 19.09.2024
+
+=begin
+RB109 ASSESSMENT PREP
+- Overview of Assessment format:
+=> Written Assessment
+    - 2 hr time limit
+    - around 11 questions
+  - Use Markdown
+  - Time management
+    - Brevity and clarity in all things, but don't leave out key details, precise terminology, or fundamental concepts
+  => Interview Assessment
+  - 30 min time limit
+  - Focus: code breakdown
+  - No documentation
+  - Communication & language precision
+- Study Tips for Assessments:
+  => Code Examples
+  => Study with other students
+    - Try Gather.town (via #the-spot on Slack)
+  => Mock test
+- Questions?
+- Practice breaking down code examples
+
+
+# What will be output by the last two lines? What concept
+# is this code snippet demonstrating?
+
+first_name = 'Alice'
+person1 = { name: first_name }
+person2 = person1
+
+p person1[:name]
+p person2[:name]
+
+## Bonus Questions ##
+# 1. What if we add `first_name.upcase!` on line 6? Does this change
+#    the output, why or why not?
+# 2. How many hashes exist in this code snippet? How many strings?
+# 3. What if we remove `first_name.upcase!` and replace it with
+#    `person2[:name].upcase!`?
+=end
+=end
