b530d4846b05b4cde4c32e5d1838b7d243cd7c9a
Iván E. Hernández T. <iamieht@gmail.com>
2024-09-27 14:40:27 +0200

    Update 25092024.rb

--- RB109/Study_Sessions/25092024.rb
+++ RB109/Study_Sessions/25092024.rb
@@ -8,3 +8,13 @@ number = 5
 sum = compute_sum(number)
 p sum
 p number
+
+=begin
+This code snippet demonstrates the use of the upto method in Ruby. Here's a description of what it does:
+1.  1.upto(number) creates a range from 1 to the value of number.
+2.  The block { |value| total += value } is executed for each number in this range.
+3.  In each iteration, value represents the current number in the range.
+4.  total += value adds the current value to a variable called total.
+This snippet is essentially summing up all integers from 1 to number and storing the result in total. It's a concise way to calculate the sum of a sequence of numbers.
+This code demonstrates the use of Ruby's Integer#upto method and block syntax, which are concepts covered in the RB101 course.
+=end
