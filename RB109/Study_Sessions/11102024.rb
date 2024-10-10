29b93c345546d3b0aac991e9d6c8291f509b7fc2
Ivan E. Hernandez T. <iamieht@gmail.com>
2024-10-10 22:08:46 +0200

    Study Session - 11.10.2024 Ex2

--- RB109/Study_Sessions/11102024.rb
+++ RB109/Study_Sessions/11102024.rb
@@ -23,6 +23,24 @@ end
 puts animal
 puts var
 
+=begin
+**Answer**
+This code outputs `cat` and raises a NameError exception on line 24
+
+On line 1, the local variable `animal` is initialized and references a string object with value `dog`.
+
+On line 3, the `loop` methos is invoked and gets passed a `do..end` block as an argument.
+
+On line 4, `animal` is reassigned to the string `ball` and on line 5, the `break` statement stops the loop execution.
+
+On line 7, the `puts` method is called with `animal` as an argument, outputting `cat`.
+
+On line 8, the `puts` method is called with `var` as an argument, raising a NameError exception as the local variable `var` is not accesible from the outer scope as it was initialized within a block.
+
+This code demonstrates the concept of local variable scope in relation to blocks after a method invocation.
+=end
+
+
 # Ex3
 x = "outer"
 2.times do
