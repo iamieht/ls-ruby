c492b04dc4dd9b9adb5752964bee791ed8444869
Iván E. Hernández T. <iamieht@gmail.com>
2024-10-11 08:32:16 +0200

    Update 11102024.rb
    Ex3

--- RB109/Study_Sessions/11102024.rb
+++ RB109/Study_Sessions/11102024.rb
@@ -47,8 +47,7 @@ puts animal
 puts var
 
 =begin
-**Answer**
-This code outputs `cat` and raises a NameError exception on line 24
+This code outputs `cat` and raises a NameError exception on line 10
 
 On line 1, the local variable `animal` is initialized and references a string object with value `dog`.
 
@@ -73,6 +72,26 @@ x = "outer"
   end
 end
 puts x
+
+=begin
+This code outputs the string `inner` on separate lines 5 times.
+
+On line 1, the local variable `x` is initialized and references a string object with value `outer`.
+
+On line 2, we call the `times` method on the Integer object `2` and we pass a `do..end` block as an argument.
+
+On line 3, `x` is reassigned to the string `inner`
+
+On line 4, we call the `times` method on `2` and we pass a `do..end` block as an argument.
+
+On line 5, we call the `puts` method with `x` as an argument, outputting `inner` 2 times.
+
+As the `times` method is called on `2` on line 2, the string `inner` is output another 2 times, for a total of 4 times.
+
+On line 8, we call the `puts` method with `x` as an argument, outputting `inner`.
+
+This code demonstrates the concept of local variable scope in relation to blocks after a method invocation, in which the variable `i` is initialized in an outer scope and reassigned inside the block.
+=end
 
 # Ex4
 MAX_VALUE = 100
